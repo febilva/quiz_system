8d1e07dae7801058fafc073b9377bb525290cbd7
febil <febil.tvr@gmail.com>
2019-05-22 16:29:59 +0530

    seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -12,8 +12,18 @@
 
 alias QuizSystem.Quiz
 
-{:ok, question} =
+{:ok, question1} =
   Quiz.create_question(%{"question" => "When did Alexander the great visited India?"})
 
-Quiz.create_option(%{"is_asnwer" => "true", "option" => "326 BC"}, question.id)
-Quiz.create_option(%{"is_asnwer" => "false", "option" => "327 BC"}, question.id)
+Quiz.create_option(%{"is_asnwer" => "true", "option" => "326 BC"}, question1.id)
+Quiz.create_option(%{"is_asnwer" => "false", "option" => "327 BC"}, question1.id)
+
+{:ok, question2} =
+  Quiz.create_question(%{"question" => "Who designed the Indian parliament Building ?"})
+
+Quiz.create_option(%{"is_asnwer" => "true", "option" => "Herbert Bekar"}, question2.id)
+
+Quiz.create_option(
+  %{"is_asnwer" => "false", "option" => "Rashtrakuta king Krishna"},
+  question2.id
+)
